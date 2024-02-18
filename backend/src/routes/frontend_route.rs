4c162942fcad8ac7283b0e92bc62f827f26c15ac
Motabhai <admin@Saumils-MacBook-Pro.local>
2024-02-18 15:49:01 +0530

    Init

---
+++ backend/src/routes/frontend_route.rs
@@ -0,0 +1,26 @@
+use salvo::prelude::*;
+use salvo::serve_static::StaticDir;
+use std::env;
+
+pub fn frontend_route() -> Router{
+
+    if env::var("env").unwrap() == "development" {
+
+        Router::with_path("<**path>").get(
+            StaticDir::new([
+                "../frontend",
+                "../frontend/public",
+            ])
+            .defaults("index.html")
+            .auto_list(true)
+        )
+    }else{
+        Router::with_path("<**path>").get(
+            StaticDir::new([
+                "./public",
+            ])
+            .defaults("index.html")
+            .auto_list(true)
+        )
+    }
+}
